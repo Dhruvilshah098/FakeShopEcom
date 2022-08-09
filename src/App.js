072fe38b5124fcccdc76f0ab42d5cec1fbdb8af0
Dhruvil Shah <dhruvil.s@enigma-tech.io>
2022-08-09 16:14:49 +0100

    route setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+
 import Header from './containers/Header';
+import ProductListing from './containers/ProductListing';
+import ProductDetail from './containers/ProductDetail';
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+     
+      <BrowserRouter>
       <Header />
+        <Routes>
+          <Route path='/' element={<ProductListing />}></Route>
+          <Route path='/product/product:id' element={<ProductDetail />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
